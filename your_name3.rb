91af24440a41be4c32caacb606487fab11d0cd64
mask0 <takashi.higasa@gmail.com>
2017-06-18 06:42:04 +0900

    tutorial essape sequence \"

--- your_name3.rb
+++ your_name3.rb
@@ -2,5 +2,5 @@ print "please type your name :"
 name = gets.chomp
 # puts "Dear " + name + ", Hello!" => commentout
 
-puts "Dear #{name}, Hello!\n"
+puts "Dear \"#{name}\", Hello!\n"
 puts "#{Time.now}"
